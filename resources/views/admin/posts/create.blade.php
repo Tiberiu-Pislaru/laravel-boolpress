d8ac71e2a080e7c668001b1e843178a08579d6fe
Tiberiu-Pislaru <tiberiupislaru03@gmail.com>
2022-03-15 12:58:31 +0100

    adds fillable and slug

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -5,12 +5,18 @@
         @csrf
         <div class="form-group">
             <label for="exampleInputEmail1">Title</label>
-            <input type="text" class="form-control" name="title">
+            <input type="text" class="form-control @error('title') is-invalid @enderror" value='{{old('title')}}' name="title">
             <small class="form-text text-muted">The post's title.</small>
+            @error('title')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
         </div>
         <div class="form-group">
             <label>Description</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="10"></textarea>
+            <textarea name='description' class="form-control  @error('description') is-invalid @enderror" id="exampleFormControlTextarea1" rows="10">{{old('description')}}</textarea>
+            @error('description')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
         </div>
         
         <button type="submit" class="btn btn-primary">Submit</button>
