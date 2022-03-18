68b82c7797b65d5aa71525bbccc9802feb1ebf8b
Tiberiu-Pislaru <tiberiupislaru03@gmail.com>
2022-03-18 13:47:03 +0100

    adds an API route and a boostrap card template

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,13 +1,50 @@
 <template>
     <div>
+        <div>
+            <a href="/login">Admin</a>
+        </div>
         <h1>Welcome in Vue</h1>
+        <div class="container-lg d-flex">
+
+            <div v-for="post in posts" :key="post.id" class="row row-cols-1 row-cols-md-2 g-4">
+                <div class="card" >
+                    <img class="card-img-top" src="http://www.asdalcione.it/wp-content/uploads/2016/08/jk-placeholder-image-1.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ post.title }}</h5>
+
+                        <p class="card-text">{{ post.description }}</p>
+                        
+                        <p class="card-text">Author: {{ post.user.name }}</p>
+                        
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        
     </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-    
-}
+    data(){
+        return {
+            posts: [],
+        }
+        
+    },
+    methods:{
+        async fetchPosts(){
+            const res = await axios.get('/api/posts');
+            this.posts = res.data;
+            console.log(res.data);
+        }
+    },
+    mounted(){
+        this.fetchPosts();
+    }
+};
 </script>
 
 <style lang="scss" scoped>
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('posts','Api\PostController');
+
+// Route::get('/posts','Api\PostController@index');
+// Route::get('/posts/{post}','Api\PostController@show');
+// Route::post('/posts','Api\PostController@store');
